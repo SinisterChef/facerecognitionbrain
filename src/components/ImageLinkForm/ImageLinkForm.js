7e78ac3fa7ff547ecfe31b03dbf13b60da587556
Kyle OBrien <kyleobis@gmail.com>
2024-03-21 14:06:55 -0400

    Adding a few examples to get start

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -6,6 +6,11 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div className='f3'>
             <p className='white text-shadow'>Dank Brain will detect human faces in your pictures. Enter a image URL below.</p>
+            <p className='white text-shadow'>Here are a few examples <br />
+            Crowd: https://bit.ly/4aner8T <br />
+            Baby: https://bit.ly/4a4EVfy <br />
+            Cat: https://bit.ly/3vt4qrS
+            </p>
             <div className='center'>
                 <div className='pa4 br3 center form'>
                     <input type='text' className='f4 pa2 w-70 center' onChange={onInputChange} />
